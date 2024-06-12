740b5e95553b853e83ff4238754a156737e6817e
Dielli Demjaha <diellidemjaha@gmail.com>
2024-06-12 10:07:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import ChatComponent from './ai-chat';
 import UniversityStats from './Universities';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
   return (
     <>
     <Router>
-    
                 <Routes>
                     <Route path="/" element={<UniversityStats />} />
                 </Routes> 
